70673aa785413bc55281a8e8c2bf84507c9c43fa
Jattin Manhas <jattinmanhas0102@gmail.com>
2023-01-28 19:53:46 +0530

    Added some more fetures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Header from "../components/header";
-import {sanityClient} from "../sanity";
+import {sanityClient, urlFor} from "../sanity";
 import {Post} from "../typing"
 
 interface Props{
@@ -41,6 +42,23 @@ const Home: NextPage<Props> = ({posts} : Props) => {
       </div>
 
       {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-4 md:p-6">
+        {posts.map((post) =>(
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+                <div className="border rounded-lg group cursor-pointer overflow-hidden">
+                    <img className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out" src={urlFor(post.mainImage).url()!} alt="Main Image" />
+                    <div className="flex justify-between p-3 bg-white">
+                      <div>
+                        <p className="font-bold text-lg">{post.title}</p>
+                        <p>{post.description} by {post.author.name}</p>
+                      </div>
+
+                      <img className="w-12 h-12 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+                    </div>
+                </div>
+            </Link>
+        ))}
+      </div>
     </div>
   );
 };
